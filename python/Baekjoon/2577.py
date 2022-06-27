86117ec7ea9121448030ba8691e039e7d5aa8bb5
Jung_Ji_Bum <2377325@naver.com>
2022-06-27 16:54:11 +0900

    UPLOAD_MAC

--- python/Baekjoon/2577.py
+++ python/Baekjoon/2577.py
@@ -2,8 +2,15 @@ A = int(input())  # 150
 B = int(input())  # 266
 C = int(input())  # 427
 
+# ls = one * two * three
+# print(ls)
+# ls1 = str(ls)
+# print(ls1)
+# ls2= list(ls1)
+# print(ls2)
 rs = list(str(A*B*C))  # 17,037,300숫자를 문자열로 리스트화
+# print(rs)
 
-for i in range(0, 10):
+for i in range(1, 10):
     print(rs.count(str(i)))
     # 0 부터 9까지 갯수 카운트
