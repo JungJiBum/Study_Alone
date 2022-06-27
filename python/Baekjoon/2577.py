6ad6041961af2e6a7187ae6db01970581e72417b
Jung_Ji_Bum <2377325@naver.com>
2022-06-27 12:22:47 +0900

    2577.py upload

--- python/Baekjoon/2577.py
+++ python/Baekjoon/2577.py
@@ -1,16 +1,9 @@
-one = int(input()) # 150
-two = int(input()) # 266
-three = int(input()) # 427
+A = int(input())  # 150
+B = int(input())  # 266
+C = int(input())  # 427
 
-# ls = one * two * three
-# print(ls)
-# ls1 = str(ls)
-# print(ls1)
-# ls2= list(ls1)
-# print(ls2)
-rs = list(str(one*two*three)) # 17,037,300숫자를 문자열로 리스트화
-print(rs)
+rs = list(str(A*B*C))  # 17,037,300숫자를 문자열로 리스트화
 
-for i in range(1,10):
+for i in range(0, 10):
     print(rs.count(str(i)))
-    #0 부터 9까지 갯수 카운트
+    # 0 부터 9까지 갯수 카운트
