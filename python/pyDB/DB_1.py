349d82b767e7a3c4ec8899227ddc6fc42f734b78
JungJiBum <2377325@naver.com>
2022-07-22 17:34:01 +0900

    upload

--- python/pyDB/DB_1.py
+++ python/pyDB/DB_1.py
@@ -1,33 +1,7 @@
 # STEP 1 임포트
 from telnetlib import IP
 import pandas as pd
-from psutil import users
 import pymysql
-
-# # STEP 2 MySQL Connection 연결
-# con = pymysql.connect(host='10.10.200.130',
-#                       user='root',
-#                       password='123qwe',
-#                       db='TEST',
-#                       charset='utf8',
-#                       cursorclass=pymysql.cursors.DictCursor)
-
-# #  STEP 3 Connection 으로부터 Cursor 생성
-# cur = con.cursor()
-
-# # STEP 4 SQL문 실행 및 Fetch
-# sql = "SELECT * FROM SIGN"
-# cur.execute(sql)
-
-# # 데이터 Fetch
-# rows = cur.fetchall()
-# # print(f"DB 값 : {rows}")
-# df_rows = pd.DataFrame(rows)
-# print(df_rows)
-
-
-# # STEP 5 DB 연결 종료
-# con.close()
 
 class DB_CONNECT:
 
@@ -44,6 +18,7 @@ class DB_CONNECT:
         print("=====접속정보=====")
         print(f"HOST : {self.host} / username : {self.username} / password : {self.password}")
     
+    # # STEP 2 MySQL Connection 연결
     def connect_db(self):
         con = pymysql.connect(host = self.host,
         user=self.username,
@@ -52,16 +27,24 @@ class DB_CONNECT:
         charset='utf8',
         cursorclass=pymysql.cursors.DictCursor)
         
+        #  STEP 3 Connection 으로부터 Cursor 생성
         cur = con.cursor()
 
+        # STEP 4 SQL문 실행 및 Fetch
         sql = "SELECT * FROM SIGN"
         cur.execute(sql)
+        # 데이터 Fetch
         rows = cur.fetchall()
         df_rows = pd.DataFrame(rows)
         print(df_rows)
+
+        # STEP 5 DB 연결 종료
         con.close()
         print("연결종료")
-        
+
+
+
+
 IP = input("입력 주소 ? : ")
 DB = input("DB 명 ? : ")
 users = input("user명 ? : ")
